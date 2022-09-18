3ace1659dfa3bf370a1b8ba72b6200c7577147ba
moin2672 <moin2672@gmail.com>
2022-09-18 11:18:00 +0530

    3. working with props and dynamic data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import Todo from './components/Todo'
+
 function App() {
   return (
     <div>
         <h1>My Todos</h1>
-        <div className="card">
-          <h2>TITLE</h2>
-          <div className="actions">
-          <button className="btn">Delete</button>
-          </div>
-        </div>
+        <Todo text="learn Rect" />
+        <Todo text="maseter React" />
+        <Todo text="explore more about react" />
+        <Todo text="doctrate React" />
     </div>
   );
 }
